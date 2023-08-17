d9653a68245f3645f3c8f28ca66cfdf886f47f07
sgc <sgcdani@gmail.comc>
2023-08-17 19:54:02 -0300

    ultimo exercicio de listas

---
+++ aulas_python_algoritmos/respostas_Listas/lista_10.py
@@ -0,0 +1,11 @@
+
+vetor1=['0','1','2','3','4','5','6','7','8','9']
+vetor2 = ['a','e','i','o','u','k','i','j','x','y']
+combinados=[]
+consoantes=[]
+caracteres_digitados=[]
+for i in range(len(vetor1)):
+       
+    combinados.append(vetor1[i])
+    combinados.append(vetor2[i])
+print(combinados) 
